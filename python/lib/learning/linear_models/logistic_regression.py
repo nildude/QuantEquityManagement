c554da2c04544da55932f4967d12069acde9c45e
RajanS <mrajancsr@gmail.com>
2020-07-11 13:46:17 -0400

    started work on logistic regression

--- python/lib/learning/linear_models/logistic_regression.py
+++ python/lib/learning/linear_models/logistic_regression.py
@@ -10,5 +10,50 @@ from typing import Dict
 
 
 class LogisticRegression(LinearBase):
-    pass
-    # todo: move the Base class to a seperate folder.  
+    """
+    Implements Logistic Regression via nls
+    Args:
+    fit_intercept: indicates if intercept is needed or not
+
+    Attributes:
+    theta:          Coefficient Weights after fitting
+    predictions:    Predicted Values from fitting
+    residuals:      Number of incorrect Predictions
+
+    Notes:
+    Class uses two estimation methods to estimate the parameters
+    of logistic regression
+    - A implemention using Newton's Method is given
+    - A implemention using Stochastic Gradient Descent is given
+    """
+
+    def __init__(self, fit_intercept: bool=True):
+        self.fit_intercept = fit_intercept
+    
+    def fit(self, X: np.ndarray, y: np.ndarray, method: str='') -> 'LogisticRegression':
+        """fits model to training data and returns regression coefficients
+        Args:
+        X: 
+            shape = (n_samples, p_features)
+            n_samples is number of instances i.e rows
+            p_features is number of features i.e columns
+        y: 
+            shape = (n_samples)
+            Target values
+
+        method: 
+            for now none
+
+        Returns:
+        object
+        """
+        pass
+
+    def predict(self):
+        pass
+
+
+
+
+    
+
