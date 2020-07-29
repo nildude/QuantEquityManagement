c0828968fc94864a5fce3adb6d2c83197a76e63e
RajanS <mrajancsr@gmail.com>
2020-07-29 00:17:13 -0400

    added some logistic regression work

--- python/lib/learning/linear_models/logistic_regression.py
+++ python/lib/learning/linear_models/logistic_regression.py
@@ -6,7 +6,7 @@ Created: May 25, 2020
 import numpy as np
 from scipy.optimize import minimize
 from learning.base import LinearBase
-from typing import Dict
+from typing import Union, Dict
 
 class LogisticRegression(LinearBase):
     """
@@ -25,8 +25,10 @@ class LogisticRegression(LinearBase):
     - A implemention using Newton's Method is given
     - A implemention using Stochastic Gradient Descent is given
     """
-    def __init__(self, fit_intercept: bool=True):
+    def __init__(self, fit_intercept: bool=True, degree: int=1):
         self.fit_intercept = fit_intercept
+        self.degree = degree 
+        self.run = False
     
     def sigmoid(self, z):
         return 1.0 / (1 + np.exp(-z))
@@ -34,6 +36,35 @@ class LogisticRegression(LinearBase):
     def sigmoid_prime(self, z):
         return self.sigmoid(z) * (1 - self.sigmoid(z))
     
+    def _jacobian(self, guess: np.ndarray, X: np.ndarray, y: np.ndarray):
+        return X @ (y - self.sigmoid(guess))
+    
+    def _hessian(self, guess: np.ndarray, X: np.ndarray, y: np.ndarray):
+        return X @ np.diag()
+    
+    def _loglikelihood(self, y, z):
+        return y @ z - np.log(1 + np.exp(z)).sum()
+    
+    def _objective_func(self, guess: np.ndarray, X: np.ndarray, y: np.ndarray):
+        """the objective function to be minimized, returns weights
+        Args:
+        guess: 
+            initial guess for optimization
+            shape = {1, p_features}
+            p_features is the numnber of columns of design matrix X
+        
+        X:
+            the design matrix
+            shape = {n_samples, n_features}
+        
+        Returns: 
+        Scalar value from loglikelihood function
+        """
+        # z = X @ theta
+        z = self.predict(X, guess=guess)
+        f = self._loglikelihood(y, z)
+        return f
+
     def fit(self, X: np.ndarray, y: np.ndarray, method: str='') -> 'LogisticRegression':
         """fits model to training data and returns regression coefficients
         Args:
@@ -53,8 +84,31 @@ class LogisticRegression(LinearBase):
         """
         pass
 
-    def predict(self):
-        pass
+    def predict(self, X: np.ndarray, 
+    thetas: Union[np.ndarray, None] = None) -> Union[np.ndarray, Dict]:
+        """makes predictions of response variable given input params
+        Args: 
+        X: 
+            shape = (n_samples, p_features)
+            n_samples is number of instances
+            p_features is number of features
+        thetas: 
+            if initialized to None: 
+                uses estimated theta from fitting process
+            if array is given: 
+                it serves as initial guess for optimization
+    
+        Returns: 
+        predicted values:
+            shape = (n_samples, 1)
+        """
+        if thetas is None:
+            if isinstance(self.theta, np.ndarray):
+                return X @ self.theta
+            else:
+                return X @ self.theta['x']
+            return X @ thetas
+        
 
 
 
